d1820c4dfc63c38be9548c5991c6c1152a7a46e3
Nalin Dwivedi <nalin.dwivedi@berkeley.edu>
2022-02-17 19:31:15 -0800

    half way thru lecture 4 code demo

---
+++ lecture/lecture_4_jscript_1/location.js
@@ -0,0 +1,9 @@
+function printHome(city, state, country) {
+    console.log(`I am from ${city}, ${state} ${country}`);
+}
+
+var city = 'West Windsor'
+var state = 'New Jersey'
+var country = 'USA'
+
+printHome(city, state, country);
