b1671b0aee0f8c88eca00f05afd3efe1b51a1f33
Nalin Dwivedi <nalin.dwivedi@berkeley.edu>
2022-03-01 10:10:44 -0800

    added lecture files

---
+++ lecture/lecture_4_jscript_1/notes/experiments.js
@@ -0,0 +1,12 @@
+// const monthlyRent = 500;
+
+// const yearlyRent = monthlyRent * 12;
+// console.log(yearlyRent);
+
+const skyIsBlue = false;
+
+if (skyIsBlue) {
+  console.log("The sky is blue!");
+} else {
+  console.log("The sky is … not blue?");
+}
