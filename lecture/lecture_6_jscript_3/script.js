faac5473b684894e0583bc0abb733abc9a92a7f2
Nalin Dwivedi <nalin.dwivedi@berkeley.edu>
2022-03-07 01:31:17 -0800

    finished lecture6 code

--- lecture/lecture_6_jscript_3/script.js
+++ lecture/lecture_6_jscript_3/script.js
@@ -1,3 +1,4 @@
+// scope
 // function greet() {
 //     console.log("Hello World");
 // }
@@ -35,17 +36,47 @@
 // start();
 // console.log(message);
 
-const video = {
-    title: 'a',
-    play() {
-        console.log(this);
-    }
-}
+
+// this keyword
+// const video = {
+//     title: 'a',
+//     play() {
+//         console.log(this);
+//     }
+// }
 
 // video.play();
 
-function playVideo() {
-    console.log(this);
-}
+// function playVideo() {
+//     console.log(this);
+// }
 
-playVideo();
+// playVideo();
+
+console.log("Hello World");
+
+var photoElement = document.getElementById("apod");
+var title = document.getElementById("title");
+var body = document.getElementById("body");
+var photoTwo = document.getElementById("nasa");
+
+fetch("https://api.nasa.gov/planetary/apod?api_key=XUgYdeMpsah8uIoRuJF1shwhJLcBhbTK89feSecC")
+    .then(
+    (response) => response.json()
+    ).then(
+    (data) => {
+        console.log(data);
+        photoElement.setAttribute("src", data.url);
+        title.innerHTML = data.title;
+        body.innerHTML = data.explanation;
+    }
+);
+
+fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=XUgYdeMpsah8uIoRuJF1shwhJLcBhbTK89feSecC").then(
+    (response) => response.json()
+).then(
+    (data) => {
+        console.log(data.photos[90]);
+        photoTwo.setAttribute("src", data.photos[90].img_src);
+    }
+);
