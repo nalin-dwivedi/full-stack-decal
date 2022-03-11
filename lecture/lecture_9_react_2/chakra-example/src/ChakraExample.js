e91bece3b49dca2a5a828270717012ded3525333
Nalin Dwivedi <nalin.dwivedi@berkeley.edu>
2022-03-10 19:46:25 -0800

    lecture 9 notes

---
+++ lecture/lecture_9_react_2/chakra-example/src/ChakraExample.js
@@ -0,0 +1,16 @@
+// import './App.css';
+import { Heading, Button } from '@chakra-ui/react'
+
+function ChakraExample() {
+
+  let x = 5;
+
+  return (
+    <>
+      <Heading>{x}</Heading>
+      <Button size="lg">Click me!</Button>  
+    </>
+  );
+}
+
+export default ChakraExample;
